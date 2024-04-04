5c643600b73a2f7e76b80a31e5030dc3070cd631
sakshi1feb <sakshi1feb@gmail.com>
2024-04-04 17:18:22 +0530

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    mobileNumber:{
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 10,
+        maxlength: 10,
+    },pasword:{
+        type: String,
+        required: true,
+    },
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
